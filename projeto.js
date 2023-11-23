817f0d8b3aa7f75f125f985b9d98fc8585699cb2
junior <junior.oliveira05@icloud.com>
2023-11-23 08:55:06 -0300

    ajustando código e testando as funçoes

--- projeto.js
+++ projeto.js
@@ -25,37 +25,40 @@ let lista = [
 ];
 
 //Adicionar uma tarefa
-function addTarefa (id, nome, status) {
+function adicionarTarefa (id, nome, status) {
   lista.push({id, nome, status})
-  return lista
 }
 
-addTarefa(2, "Estudar", "Em andamento")
-addTarefa(3, "Compras", "Pendente")
+adicionarTarefa(2, "Estudar", "Em andamento")
+adicionarTarefa(3, "Compras", "Pendente")
 
 console.log(lista)
+
 //Editar uma tarefa salva
 function editarTarefa(nome, editar) {
-  for(i in lista) {
-    if (nome === lista[i].nome){
-      lista[i].nome = editar
-      return lista
+  for (let tarefa of lista) {
+    if (nome === tarefa.nome){
+      tarefa.nome = editar;
+      return lista;
     }
   }
-  
+  return `Não existe essa tarefa: ${nome} na lista `;
 }
 
+//editarTarefa("nome da tarefa salva", "nome para a nova tarefa"))
+
+console.log(editarTarefa("Estudar", "Meditar"))
 console.log(editarTarefa("Estudar", "Meditar"))
 
 //Remover uma tarefa salva
-function removerTarefa (nome) {
-  for(i in lista) {
-    if(nome === lista[i].nome) {
-      lista.splice(i, 1)
-      return `Tarefa ${nome}, removida!`
+function removerTarefa(nome) {
+  for (let [i, tarefa] of lista.entries()) {
+    if (nome === tarefa.nome) {
+      lista.splice(i, 1);
+      return `Tarefa ${nome}, removida!`;
     }
   }
-  return `Tarefa ${nome}, não existe na sua lista de tarefas `
+  return `Tarefa ${nome}, não existe na sua lista de tarefas.`;
 }
 
 removerTarefa('Estudar')
@@ -76,13 +79,14 @@ listarTarefasSalva(lista)
 function idTarefa (id) {
   for(i in lista) {
     if(id === lista[i].id) {
-      return console.log(`ID: ${id} Nome: ${lista[i].nome} Status: ${lista[i].status}`)
+      return `ID: ${id} Nome: ${lista[i].nome} Status: ${lista[i].status}`
     }
   }
-  return console.log(`Tarefa com o ID: ${id}, não existe na sua lista de tarefas `)
+  return `Tarefa com o ID: ${id}, não existe na sua lista de tarefas `
 
 }
 
-idTarefa(3)
-idTarefa(1)
-idTarefa(2)
+console.log(idTarefa(3))
+console.log(idTarefa(1))
+console.log(idTarefa(2))
+
